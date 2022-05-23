d5f6e6fce23cf44b10ed4e9a7ef7f31832197c6b
m.azadbar <mohammadazadbar1373@gmail.com>
2022-05-23 11:28:08 +0430

    add Part14-BaseSongAdapter

--- app/src/main/java/com/mvvmspotifyyt/adapters/SwipeSongAdapter.kt
+++ app/src/main/java/com/mvvmspotifyyt/adapters/SwipeSongAdapter.kt
@@ -12,18 +12,16 @@ import com.mvvmspotifyyt.data.entities.Song
 import kotlinx.android.synthetic.main.list_item.view.*
 import javax.inject.Inject
 
-class SongAdapter @Inject constructor(
-    private val glide: RequestManager
-) : BaseSongAdapter(R.layout.list_item) {
+class SwipeSongAdapter : BaseSongAdapter(R.layout.list_item) {
 
     override val differ = AsyncListDiffer(this, diffCallback)
 
     override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
         val song = songs[position]
         holder.itemView.apply {
-            tvPrimary.text = song.title
-            tvSecondary.text = song.subtitle
-            glide.load(song.imageUrl).into(ivItemImage)
+
+            val text = "${song.title} - ${song.subtitle}"
+            tvPrimary.text = text
 
             setItemClickListener {
                 onItemClickListener?.let { click ->
